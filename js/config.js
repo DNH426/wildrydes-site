fd4247510cad71eb8cbf947e6ac858e595222e7e
dhr2505 <daniel.hagy@northwestern.edu>
2020-09-23 15:47:44 -0500

    added cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_wLJdYeNey', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4mmgou2u54imncdr11vm4ddgjk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://master.d12b869aqc9w1o.amplifyapp.com' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
